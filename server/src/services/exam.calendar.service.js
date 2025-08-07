b5023ddbe98c16c24f8cdee72efb31168d0d6e1d
DucAnh <anhnguyen2k373@gmail.com>
2025-08-07 21:01:15 +0700

    fix

--- server/src/services/exam.calendar.service.js
+++ server/src/services/exam.calendar.service.js
@@ -163,6 +163,27 @@ const queryExamSchedule = async (filter, options) => {
 //   return { inserted, skipped };
 // };
 
+// Hàm chuyển serial Excel → JavaScript Date
+function excelDateToJSDate(serial) {
+  const utc_days = Math.floor(serial - 25569);
+  const utc_value = utc_days * 86400; // seconds
+  const date_info = new Date(utc_value * 1000);
+
+  const fractional_day = serial - Math.floor(serial) + 0.0000001;
+  let total_seconds = Math.floor(86400 * fractional_day);
+
+  const seconds = total_seconds % 60;
+  total_seconds -= seconds;
+  const hours = Math.floor(total_seconds / (60 * 60));
+  const minutes = Math.floor((total_seconds - hours * 3600) / 60);
+
+  date_info.setHours(hours);
+  date_info.setMinutes(minutes);
+  date_info.setSeconds(seconds);
+
+  return date_info;
+}
+
 const importFromExcel = async (filePath) => {
   const workbook = xlsx.readFile(filePath);
   const sheet = workbook.Sheets[workbook.SheetNames[0]];
